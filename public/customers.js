600f70afb0f830b5fc34a9b64eb4f1050f0f3572
Kevin <tpnxyjr@gmail.com>
2017-02-28 18:01:46 -0800

    15 unfinished

--- public/customers.js
+++ public/customers.js
@@ -11,10 +11,139 @@ customers.use(passport.session());
 customers.use(express.static( 'public'));
 
 customers.get('/home',function(req,res){
-    res.render('customerhome',{user: req.user});
+    var data="<thead><tr><th>Order Number</th><th>Order Date</th><th>Order Type</th><th>PO Number</th><th>Order Status</th></tr></thead></tbody>";
+    if(req.user){
+        sql.execute({
+            query: sql.fromFile("./sql/getCustomerOrders.sql"),
+            params: {cusno: req.user.custid}
+        }).then(function(result){
+            for(var i = 0; i < result.length; i++){
+                data = data + "<tr><td>"+result[i].ord_no+"</td><td>"+result[i].ord_dt.toString().substring(0,16)+"</td><td>"+result[i].ord_type+"</td><td>"+result[i].oe_po_no+"</td><td>"+result[i].status+"</td></tr>"
+            }
+            data = data + "</tbody>"
+            res.render('customerhome',{user: req.user, data: data});
+        });
+    }
+    else res.render('customerhome',{user: req.user, data: "You have no open orders"});
 });
 customers.get('/RegularOrder',function(req,res){
    res.render('RegularOrder',{user: req.user})
+});
+customers.post('/RegularOrder',function(req,res){
+
+});
+customers.get('/getItem',function(req,res){
+    var itemno = req.query.itemno;
+
+    sql.execute({
+        query: sql.fromFile("./sql/getItemInfo.sql"),
+        params:{itemno: itemno}
+    }).then(function (result){
+        if(result[0] == null){
+            var data = [];
+            data.push({
+                "item_desc": "Not A Valid Item",
+                "uom": ""
+            });
+            res.end(JSON.stringify(data));
+        }
+        else {
+            var data = [];
+            data.push({
+                "item_desc": result[0].item_desc_1,
+                "uom": result[0].uom
+            });
+            res.end(JSON.stringify(data));
+        }
+    },function (err) {
+        console.log(err);
+    });
+});
+customers.get('/getPrice',function(req,res){
+    sql.execute({
+        query: sql.fromFile("./sql/getItemPrice.sql"),
+        params:{ itemno : req.query.itemno}
+    }).then(function (result){
+        if(result == null){
+            data.push({
+                "baseprice" : "Price Not Found",
+                "totalprice" : 0
+            });
+            res.end(JSON.stringify(data));
+        }
+        else{
+            var baseprice = result[0].price;
+
+            sql.execute({
+                query: sql.fromFile("./sql/Code68.sql"),
+                params:{ itemno : req.query.itemno, code : '6', cat : ''}
+            }).then(function(result){
+               if(result == null){
+                   sql.execute({
+                       query: sql.fromFile("./sql/Code68.sql"),
+                       params:{ itemno : req.query.itemno, code: '8', cat : ''}
+                   }).then(function(result) {
+                        if(result == null){
+                            sql.execute({
+                                query: sql.fromFile("./sql/getDiscount.sql"),
+                                params:{ itemno : req.query.itemno}
+                            }).then(function(result) {
+                                // code 1,2,3,4,5,6,7
+                                if(result == null){
+                                    sql.execute({
+                                        query: sql.fromFile("./sql/getCustomerCode.sql"),
+                                        params:{ itemno : req.query.itemno}
+                                    }).then(function(result) {
+
+                                    });
+                                }
+                                var discount = 0;
+                                if(result[0].minimum_qty_5 != 0 && req.query.qty > result[0].minimum_qty_5) discount = result[0].prc_or_disc_5;
+                                else if(result[0].minimum_qty_4 != 0 && req.query.qty > result[0].minimum_qty_4) discount = result[0].prc_or_disc_4;
+                                else if(result[0].minimum_qty_3 != 0 && req.query.qty > result[0].minimum_qty_3) discount = result[0].prc_or_disc_3;
+                                else if(result[0].minimum_qty_2 != 0 && req.query.qty > result[0].minimum_qty_2) discount = result[0].prc_or_disc_2;
+                                else if(req.query.qty > result[0].minimum_qty_1) discount = result[0].prc_or_disc_1;
+                                data.push({
+                                    "baseprice" : baseprice*discount*0.01,
+                                    "totalprice" : baseprice*discount*0.01*req.query.qty
+                                });
+                                res.end(JSON.stringify(data));
+                            });
+                        }
+                       else{
+                            //code 8
+                            var discount = 0;
+                            if(result[0].minimum_qty_5 != 0 && req.query.qty > result[0].minimum_qty_5) discount = result[0].prc_or_disc_5;
+                            else if(result[0].minimum_qty_4 != 0 && req.query.qty > result[0].minimum_qty_4) discount = result[0].prc_or_disc_4;
+                            else if(result[0].minimum_qty_3 != 0 && req.query.qty > result[0].minimum_qty_3) discount = result[0].prc_or_disc_3;
+                            else if(result[0].minimum_qty_2 != 0 && req.query.qty > result[0].minimum_qty_2) discount = result[0].prc_or_disc_2;
+                            else if(req.query.qty > result[0].minimum_qty_1) discount = result[0].prc_or_disc_1;
+                            data.push({
+                                "baseprice" : baseprice*discount*0.01,
+                                "totalprice" : baseprice*discount*0.01*req.query.qty
+                            });
+                            res.end(JSON.stringify(data));
+                        }
+                   });
+               }
+                else{
+                   //code 6
+                   var discount = 0;
+                   if(result[0].minimum_qty_5 != 0 && req.query.qty > result[0].minimum_qty_5) discount = result[0].prc_or_disc_5;
+                   else if(result[0].minimum_qty_4 != 0 && req.query.qty > result[0].minimum_qty_4) discount = result[0].prc_or_disc_4;
+                   else if(result[0].minimum_qty_3 != 0 && req.query.qty > result[0].minimum_qty_3) discount = result[0].prc_or_disc_3;
+                   else if(result[0].minimum_qty_2 != 0 && req.query.qty > result[0].minimum_qty_2) discount = result[0].prc_or_disc_2;
+                   else if(req.query.qty > result[0].minimum_qty_1) discount = result[0].prc_or_disc_1;
+                   data.push({
+                       "baseprice" : baseprice*(100-discount)*0.01,
+                       "totalprice" : baseprice*(100-discount)*0.01*req.query.qty
+                   });
+                   res.end(JSON.stringify(data));
+               }
+            });
+        }
+
+    });
 });
 customers.get('/search',function(req,res){
     sql.execute({
@@ -29,6 +158,9 @@ customers.get('/search',function(req,res){
     },function (err) {
         console.log(err);
     });
+});
+customers.get('/iteminfo',function(req,res){
+
 });
 exports.customers = customers;
 module.exports= customers;
--- public/js/regorder.js
+++ public/js/regorder.js
@@ -17,15 +17,51 @@ function addRow(tableID) {
             script.type = "text/javascript";
 
             var scriptString = "$('#"+idstring+"').autocomplete({ source: function(req,res) {  $.ajax({   url: '/customers/search?key='+req.term,dataType: 'json',type: 'GET',"+
-                    "data: {  term: req.term }, success: function(data) { res($.map(data, function(item) { return { label: item,  value: item };}));},error: function(xhr){alert(xhr.status + ' : ' + xhr.statusText);}"+
+                "data: {  term: req.term }, success: function(data) { res($.map(data, function(item) { return { label: item,  value: item };}));},error: function(xhr){alert(xhr.status + ' : ' + xhr.statusText);}"+
                 "}); },  select: function(event, ui) {  } });";
 
             script[(script.innerText===undefined?"textContent":"innerText")] = scriptString;
-            newcell.innerHTML = "<input type='text' name='typeahead' id='"+idstring+"' class='typeahead tt-query' autocomplete='off' spellcheck='false' placeholder='Item Number'>";
+            newcell.innerHTML = "<input type='text' name='typeahead' id='"+idstring+"' class='typeahead tt-query' autocomplete='off' spellcheck='false' placeholder='Item Number' onchange='autofill("+rowCount+","+colCount+")'>";
             newcell.appendChild(script);
         }
         else if(i==2){
-            newcell.innerHTML = "<input type = 'text' id='"+idstring+"' placeholder='Qty'>";
+            newcell.innerHTML = "<input type = 'text' id='"+idstring+"' placeholder='Qty' onchange='autofill("+rowCount+","+colCount+")'>";
+        }
+        else{
+            newcell.innerHTML = "<input type = 'text' id='"+idstring+"' readonly>";
+        }
+    }
+}
+function addDuplicate(tableID) {
+
+    var table = document.getElementById(tableID);
+
+    var rowCount = table.rows.length;
+    var row = table.insertRow(rowCount);
+
+    var colCount = table.rows[0].cells.length;
+    for (var i = 0; i < colCount; i++) {
+        var newcell	= row.insertCell(i);
+        var idstring = "inside"+rowCount+"at"+i;
+
+        var val = "";
+        if(rowCount > 1) val = document.getElementById("inside" + (rowCount-1) + "at" + i).value;
+        if(i==0){
+        }
+        else if(i==1){
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+
+            var scriptString = "$('#"+idstring+"').autocomplete({ source: function(req,res) {  $.ajax({   url: '/customers/search?key='+req.term,dataType: 'json',type: 'GET',"+
+                "data: {  term: req.term }, success: function(data) { res($.map(data, function(item) { return { label: item,  value: item };}));},error: function(xhr){alert(xhr.status + ' : ' + xhr.statusText);}"+
+                "}); },  select: function(event, ui) {  });";
+
+            script[(script.innerText===undefined?"textContent":"innerText")] = scriptString;
+            newcell.innerHTML = "<input type='text' name='typeahead' id='"+idstring+"' class='typeahead tt-query' autocomplete='off' spellcheck='false' value=\""+escapeHtml(val)+"\" placeholder='Item Number'>";
+            newcell.appendChild(script);
+        }
+        else if(i==2){
+            newcell.innerHTML = "<input type = 'text' id='"+idstring+"' placeholder='Qty' onchange='autofill("+rowCount+","+colCount+")'>";
         }
         else{
             newcell.innerHTML = "<input type = 'text' id='"+idstring+"' readonly>";
@@ -45,7 +81,51 @@ function deleteRow(tableID) {
         alert(e);
     }
 }
-function autofill(i){
-
+function autofill(rowCount,colCount){
+        var idstring = "inside" + rowCount + "at";
+        var item = document.getElementById(idstring+1).value;
+        jQuery(function($) {
+            $.ajax({
+                data: { itemno: item},
+                dataType: 'json',
+                type: 'GET',
+                url: '/customers/getItem',
+                success: function(data) {
+                    $.each(data, function(index, element) {
+                        document.getElementById(idstring+3).value = element.uom;
+                        document.getElementById(idstring+5).value = element.item_desc;
+                    });
+                }
+                ,error: function(xhr){alert(xhr.status + ' : ' + xhr.statusText);
+                     }
+            });
+        });
+        if(document.getElementById(idstring+2).value!='' && !isNaN(document.getElementById(idstring+2).value)){
+            $.ajax({
+                data: { itemno: item, qty: document.getElementById(idstring+2).value },
+                dataType: 'json',
+                type: 'GET',
+                url: '/customers/getPrice',
+                success: function(data) {
+                    $.each(data, function(index, element) {
+                        document.getElementById(idstring+6).value = element.baseprice;
+                        document.getElementById(idstring+7).value = element.totalprice;
+                    });
+                }
+                ,error: function(xhr){alert(xhr.status + ' : ' + xhr.statusText);
+                     }
+            });
+        }
 }
-
+function subtotal(tableID){
+    var table = document.getElementById(tableID);
+    var rowCount = table.rows.length;
+    var total = 0;
+    for(var i = 1; i < rowCount; i++){
+        total+= parseInt(document.getElementById("inside"+i+"at"+7).value);
+    }
+    document.getElementById("total").value = total;
+}
+$(document).ready(function() {
+    $('#loadmsg').hide();
+});
